77e3d3af9adca3a19d1b01ddb1e170d16bb91f43
Soibi Nancy <39niches@gmail.com>
2024-05-07 01:10:22 -0500

    Exercise complete

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 10;
+            var response = answer <= 9 ? $"{answer} is less than nine or equal to nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
+
     }
 }
